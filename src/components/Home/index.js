4859566d8e97403d1c84e0d965149f90050d1070
root <munnangihimadheer27@gmail.com>
2022-09-16 08:07:15 +0000

    login

---
+++ src/components/Home/index.js
@@ -0,0 +1,37 @@
+// Write your code here
+import {Component} from 'react'
+
+import './index.css'
+
+import Message from '../Message'
+
+import Login from '../Login'
+
+import Logout from '../Logout'
+
+class Home extends Component {
+  state = {isLoggedIn: false}
+
+  onClicking = () => {
+    this.setState(prevState => ({isLoggedIn: !prevState.isLoggedIn}))
+  }
+
+  render() {
+    const {isLoggedIn} = this.state
+
+    return (
+      <div className="bg-container">
+        <div className="card-container">
+          <Message isLoggedIn={isLoggedIn} />
+          {isLoggedIn ? (
+            <Logout logout={this.onClicking} />
+          ) : (
+            <Login login={this.onClicking} />
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
